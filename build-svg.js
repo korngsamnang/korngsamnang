7105b148fc2c3464fbebf4df4f5054bc513b56e5
Korng Samnang <korngsamnang168@gmail.com>
2025-08-24 15:28:02 +0700

    Update to correct timezone

--- build-svg.js
+++ build-svg.js
@@ -1,9 +1,18 @@
 import fs from "fs";
 
-const today = new Date();
-const todayDay = new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(
-    today
-);
+const now = new Date();
+
+const todayDay = new Intl.DateTimeFormat("en-US", {
+    weekday: "long",
+    timeZone: "Asia/Bangkok",
+}).format(now);
+
+const todayDate = new Intl.DateTimeFormat("en-CA", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    timeZone: "Asia/Bangkok",
+}).format(now);
 
 const dayBubbleWidths = {
     Monday: 235,
@@ -21,8 +30,10 @@ fs.readFile("template.svg", "utf-8", (err, data) => {
     data = data.replace("{todayDay}", todayDay);
     data = data.replace("{dayBubbleWidth}", dayBubbleWidths[todayDay]);
 
+    data = data.replace("{todayDate}", todayDate);
+
     fs.writeFile("chat.svg", data, (err) => {
         if (err) return console.error(err);
-        console.log("chat.svg generated successfully with todayDay:", todayDay);
+        console.log("chat.svg generated successfully:", todayDay, todayDate);
     });
 });
